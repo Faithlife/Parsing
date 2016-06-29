8ef753c7fcee3fbb20df2d3ea43b8631f7214ce5
Ed Ball <ejball@gmail.com>
2016-06-29 15:39:58 -0700

    Support .NET Core.

--- src/Faithlife.Parsing/Properties/AssemblyInfo.cs
+++ src/Faithlife.Parsing/Properties/AssemblyInfo.cs
@@ -5,11 +5,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Faithlife.Parsing")]
 [assembly: AssemblyProduct("Faithlife.Parsing")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyCompany("Faithlife Corporation")]
-[assembly: AssemblyCopyright("Copyright 2016 Faithlife Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
--- tests/Faithlife.Parsing.Tests/Properties/AssemblyInfo.cs
+++ tests/Faithlife.Parsing.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Faithlife.Parsing.Tests")]
 [assembly: AssemblyProduct("Faithlife.Parsing.Tests")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyCompany("Faithlife")]
-[assembly: AssemblyCopyright("Copyright 2015 Faithlife")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
