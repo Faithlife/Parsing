3465cbe7a51e6f765a40df4827cf7cc1c7ed2b04
Ed Ball <ejball@gmail.com>
2017-01-31 13:40:21 -0800

    Move docs to website.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyCompany("Faithlife Corporation")]
-[assembly: AssemblyCopyright("Copyright 2016 Faithlife Corporation")]
+[assembly: AssemblyCopyright("Copyright 2016-2017 Faithlife Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
